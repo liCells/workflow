6f11b7481700028fa5ab4100967eaf75f42f691b
LZ <lizong1204@gmail.com>
2022-08-26 23:51:27 +0800

    feat: add spring bean scan

---
+++ src/main/java/org/lz/workflow/config/WorkflowConfig.java
@@ -0,0 +1,12 @@
+package org.lz.workflow.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * workflow config
+ *
+ * @author lz
+ */
+@ComponentScan("org.lz")
+public class WorkflowConfig {
+}
