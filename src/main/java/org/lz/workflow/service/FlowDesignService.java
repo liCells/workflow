0e11cb71a25cb1cadb5a77d4cf255542c82e5dc7
LZ <lizong1204@gmail.com>
2022-08-29 20:22:06 +0800

    feat: add `inspectWorkflowJson` method

--- src/main/java/org/lz/workflow/service/FlowDesignService.java
+++ src/main/java/org/lz/workflow/service/FlowDesignService.java
@@ -1,7 +1,10 @@
 package org.lz.workflow.service;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.lz.workflow.basic.Node;
+import org.lz.workflow.basic.NodeType;
 import org.lz.workflow.domain.FlowDesign;
+import org.lz.workflow.domain.Workflow;
 import org.lz.workflow.mapper.FlowDesignMapper;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +22,13 @@ public class FlowDesignService extends ServiceImpl<FlowDesignMapper, FlowDesign>
         Objects.requireNonNull(flowDesign);
 
         return flowDesign;
+    }
+
+    public void inspectWorkflowJson(Workflow workflow) {
+        Node start = workflow.getMap();
+        if (start.getType() != NodeType.START) {
+            throw new IllegalArgumentException("The workflow json must start with a start node.");
+        }
+        start.inspect();
     }
 }
