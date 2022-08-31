308548aa1b89e0ae112fe1f0670d822d48aa2789
LZ <lizong1204@gmail.com>
2022-08-31 17:53:37 +0800

    refactor: adjust `NodeType`

--- src/main/java/org/lz/workflow/basic/NodeType.java
+++ src/main/java/org/lz/workflow/basic/NodeType.java
@@ -4,13 +4,34 @@ package org.lz.workflow.basic;
  * @author lz
  */
 public enum NodeType {
-    START,
-    END,
-    USUAL_TASK,
-    PARALLEL_TASK,
-    SERIAL_TASK,
-    SINGLE_ENDED,
-    DOUBLE_ENDED,
-    AUTO_TASK,
-    TIMER_TASK,
+    START("START"),
+    END("END"),
+    USUAL_TASK("USUAL_TASK"),
+    PARALLEL_TASK("PARALLEL_TASK"),
+    SERIAL_TASK("SERIAL_TASK"),
+    SINGLE_ENDED("SINGLE_ENDED"),
+    DOUBLE_ENDED("DOUBLE_ENDED"),
+    AUTO_TASK("AUTO_TASK"),
+    TIMER_TASK("TIMER_TASK"),
+    NULL("NULL");
+
+    private final String name;
+
+    NodeType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static NodeType getByValue(String value) {
+        for (NodeType type : values()) {
+            if (type.getName().equals(value)) {
+                return type;
+            }
+        }
+        return NULL;
+    }
+
 }
