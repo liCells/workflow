a24f249f426975a5a5c703e8323efae0f5670528
LZ <lizong1204@gmail.com>
2022-10-13 12:44:11 +0800

    refactor: supplement the `startFlow` method

--- src/main/java/org/lz/workflow/service/FlowTaskService.java
+++ src/main/java/org/lz/workflow/service/FlowTaskService.java
@@ -5,6 +5,8 @@ import org.lz.workflow.basic.FlowCommonEnum;
 import org.lz.workflow.basic.Node;
 import org.lz.workflow.basic.NodeType;
 import org.lz.workflow.basic.cache.NodeMap;
+import org.lz.workflow.domain.map.Line;
+import org.lz.workflow.domain.map.UserTaskNode;
 import org.lz.workflow.domain.running.RunningTask;
 import org.lz.workflow.event.StartFlowEvent;
 import org.lz.workflow.mapper.FlowTaskMapper;
@@ -12,7 +14,6 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.util.HashMap;
 
 /**
@@ -22,32 +23,42 @@ import java.util.HashMap;
 public class FlowTaskService extends ServiceImpl<FlowTaskMapper, RunningTask> {
     private final FlowCommonService flowCommonService;
 
-    public FlowTaskService(FlowCommonService flowCommonService) {
+    private final LineService lineService;
+
+    public FlowTaskService(FlowCommonService flowCommonService, LineService lineService) {
         this.flowCommonService = flowCommonService;
+        this.lineService = lineService;
     }
 
     @Transactional
     @EventListener(StartFlowEvent.class)
     public void startFlow(StartFlowEvent startFlowEvent) {
+        // Get id.
         Long id = flowCommonService.getIdAndIncr(FlowCommonEnum.TASK_KEY);
-        final RunningTask task = new RunningTask();
-        task.setId(id);
-        task.setFlowId(startFlowEvent.getFlowId());
-        task.setFlowSymbol(startFlowEvent.getFlowSymbol());
-        task.setStartTime(LocalDate.now());
 
-        // TODO parse flow design and save to task
+        // Init running task.
+        final RunningTask task = new RunningTask(id, startFlowEvent.getFlowId(), startFlowEvent.getFlowSymbol());
+
         HashMap<String, Node> nodeHashMap = NodeMap.get(startFlowEvent.getFlowSymbol(), startFlowEvent.getFlowVersion());
-        Node startNode = nodeHashMap.get(NodeType.START.getName());
-        // TODO get next task node
-        saveTask(task);
-    }
 
-    public void parseTask() {
-
-    }
-
-    private void saveTask(RunningTask task) {
+        // Get next task node, and save to task.
+        for (Node nextLine : NodeMap.getNextNodes(nodeHashMap, NodeType.START.getName())) {
+            Line line = (Line) nextLine;
+            if (lineService.checkLine(line)) {
+                Node nextTaskNode = NodeMap.getNextNodes(nodeHashMap, line.getSymbol()).get(0);
+                if (nextTaskNode instanceof UserTaskNode) {
+                    UserTaskNode userTaskNode = (UserTaskNode) nextTaskNode;
+                    task.setName(nextTaskNode.getName());
+                    task.setNodeSymbol(userTaskNode.getSymbol());
+                    task.setType(userTaskNode.getType().getName());
+                    // TODO determine whether parsing is required
+                    task.setExecutor(userTaskNode.getExecutor());
+                    break;
+                }
+                // TODO other node type
+            }
+        }
         save(task);
     }
+
 }
