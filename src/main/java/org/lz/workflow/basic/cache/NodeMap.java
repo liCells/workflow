70e342c367db29533feb9e7875edd736c6950081
LZ <lizong1204@gmail.com>
2022-10-13 12:42:34 +0800

    feat: add the method of get the next node

--- src/main/java/org/lz/workflow/basic/cache/NodeMap.java
+++ src/main/java/org/lz/workflow/basic/cache/NodeMap.java
@@ -3,6 +3,8 @@ package org.lz.workflow.basic.cache;
 import org.lz.workflow.basic.Node;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author lz
@@ -16,5 +18,29 @@ public class NodeMap {
 
     public static void put(String symbol, int version, HashMap<String, Node> nodeMap) {
         NODE_MAP.put(symbol + "::" + version, nodeMap);
+    }
+
+    public static List<Node> getNextNodes(String symbol, int version, String nodeSymbol) {
+        return getNextNodes(symbol, version, nodeSymbol, false);
+    }
+
+    public static List<Node> getNextNodes(String symbol, int version, String nodeSymbol, boolean isTask) {
+        HashMap<String, Node> nodeHashMap = get(symbol, version);
+        return getNextNodes(nodeHashMap, nodeSymbol, isTask);
+    }
+
+    public static List<Node> getNextNodes(HashMap<String, Node> nodeHashMap, String nodeSymbol) {
+        return getNextNodes(nodeHashMap, nodeSymbol, false);
+    }
+
+    public static List<Node> getNextNodes(HashMap<String, Node> nodeHashMap, String nodeSymbol, boolean isTask) {
+        Node node = nodeHashMap.get(nodeSymbol);
+
+        Objects.requireNonNull(node, "Node not found.");
+
+        if (isTask) {
+            return node.getNextNodes(nodeHashMap, true);
+        }
+        return node.getNextNodes(nodeHashMap);
     }
 }
