809bda7ebae43d94c73f8af31e218c4bba87db36
LZ <lizong1204@gmail.com>
2022-10-14 18:16:37 +0800

    feat: add complete task method and adjust the start flow method

--- src/main/java/org/lz/workflow/mapper/FlowTaskMapper.java
+++ src/main/java/org/lz/workflow/mapper/FlowTaskMapper.java
@@ -2,6 +2,8 @@ package org.lz.workflow.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.lz.workflow.domain.history.HistoryTask;
 import org.lz.workflow.domain.running.RunningTask;
 
 /**
@@ -10,4 +12,15 @@ import org.lz.workflow.domain.running.RunningTask;
 @Mapper
 public interface FlowTaskMapper extends BaseMapper<RunningTask> {
 
+    void saveRunningTask(RunningTask task);
+
+    void saveHistoryTask(HistoryTask task);
+
+    void endHistoryTask(@Param("taskId") Long taskId);
+
+    int selectRunningTaskCountByFlowId(@Param("flowId") Long flowId);
+
+    void endFlow(Long flowId);
+
+    void deleteRunningFlow(Long flowId);
 }
--- src/main/java/org/lz/workflow/service/FlowTaskService.java
+++ src/main/java/org/lz/workflow/service/FlowTaskService.java
@@ -5,6 +5,7 @@ import org.lz.workflow.basic.FlowCommonEnum;
 import org.lz.workflow.basic.Node;
 import org.lz.workflow.basic.NodeType;
 import org.lz.workflow.basic.cache.NodeMap;
+import org.lz.workflow.domain.history.HistoryTask;
 import org.lz.workflow.domain.map.Line;
 import org.lz.workflow.domain.map.UserTaskNode;
 import org.lz.workflow.domain.running.RunningTask;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * @author lz
@@ -33,32 +35,99 @@ public class FlowTaskService extends ServiceImpl<FlowTaskMapper, RunningTask> {
     @Transactional
     @EventListener(StartFlowEvent.class)
     public void startFlow(StartFlowEvent startFlowEvent) {
+        HashMap<String, Node> nodeHashMap = NodeMap.get(startFlowEvent.getFlowSymbol(), startFlowEvent.getFlowVersion());
+
+        Objects.requireNonNull(nodeHashMap, "Flow not exist.");
+
+        // Init running task.
+        final RunningTask task = new RunningTask(
+                startFlowEvent.getFlowId(),
+                startFlowEvent.getFlowSymbol(),
+                startFlowEvent.getFlowVersion(),
+                NodeType.START.getName()
+        );
+
+        complete(nodeHashMap, task, true);
+    }
+
+    @Transactional
+    public void complete(String taskId) {
+        RunningTask runningTask = baseMapper.selectById(taskId);
+
+        Objects.requireNonNull(runningTask, "Task not exist.");
+
+        HashMap<String, Node> nodeHashMap = NodeMap.get(runningTask.getFlowSymbol(), runningTask.getVersion());
+
+        complete(nodeHashMap, runningTask, false);
+    }
+
+    private void complete(HashMap<String, Node> nodeHashMap, RunningTask runningTask, boolean isStart) {
         // Get id.
         Long id = flowCommonService.getIdAndIncr(FlowCommonEnum.TASK_KEY);
 
-        // Init running task.
-        final RunningTask task = new RunningTask(id, startFlowEvent.getFlowId(), startFlowEvent.getFlowSymbol());
+        final RunningTask task;
+        if (isStart) {
+            // Set primary key.
+            runningTask.setId(id);
+            task = runningTask;
+        } else {
+            // Init running task.
+            task = new RunningTask(id, runningTask.getFlowId(), runningTask.getFlowSymbol(), runningTask.getVersion());
+        }
 
-        HashMap<String, Node> nodeHashMap = NodeMap.get(startFlowEvent.getFlowSymbol(), startFlowEvent.getFlowVersion());
-
+        boolean isEnd = false;
         // Get next task node, and save to task.
-        for (Node nextLine : NodeMap.getNextNodes(nodeHashMap, NodeType.START.getName())) {
+        for (Node nextLine : NodeMap.getNextNodes(nodeHashMap, runningTask.getNodeSymbol())) {
             Line line = (Line) nextLine;
             if (lineService.checkLine(line)) {
-                Node nextTaskNode = NodeMap.getNextNodes(nodeHashMap, line.getSymbol()).get(0);
-                if (nextTaskNode instanceof UserTaskNode) {
-                    UserTaskNode userTaskNode = (UserTaskNode) nextTaskNode;
-                    task.setName(nextTaskNode.getName());
+                Node nextNode = NodeMap.getNextNodes(nodeHashMap, line.getSymbol()).get(0);
+                if (nextNode instanceof UserTaskNode) {
+                    UserTaskNode userTaskNode = (UserTaskNode) nextNode;
+                    task.setName(userTaskNode.getName());
                     task.setNodeSymbol(userTaskNode.getSymbol());
                     task.setType(userTaskNode.getType().getName());
                     // TODO determine whether parsing is required
                     task.setExecutor(userTaskNode.getExecutor());
+                    // Save new task.
+                    saveTask(task);
+                    break;
+                }
+                if (nextNode.getType() == NodeType.END) {
+                    // Mark flow the end.
+                    isEnd = true;
                     break;
                 }
                 // TODO other node type
             }
         }
-        save(task);
+
+        if (!isStart) {
+            // If is running, so delete running task and end history task.
+            baseMapper.deleteById(runningTask.getId());
+            baseMapper.endHistoryTask(runningTask.getId());
+        }
+
+        if (isEnd) {
+            int runningTaskCount = baseMapper.selectRunningTaskCountByFlowId(runningTask.getFlowId());
+            if (runningTaskCount < 1) {
+                // If all task is end, so end flow.
+                baseMapper.deleteRunningFlow(runningTask.getFlowId());
+                baseMapper.endFlow(runningTask.getFlowId());
+            }
+        }
     }
 
+    private void saveTask(RunningTask runningTask) {
+        HistoryTask historyTask = new HistoryTask(runningTask);
+        saveHistoryTask(historyTask);
+        saveRunningTask(runningTask);
+    }
+
+    private void saveRunningTask(RunningTask task) {
+        baseMapper.saveRunningTask(task);
+    }
+
+    private void saveHistoryTask(HistoryTask task) {
+        baseMapper.saveHistoryTask(task);
+    }
 }
